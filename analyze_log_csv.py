51837e285385da76ffcd6396132fbac4b226270a
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-02-01 19:40:26 +0800

    update analysis script
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- analyze_log_csv.py
+++ analyze_log_csv.py
@@ -5,13 +5,22 @@ from ase.units import kcal, mol
 
 import argparse
 
+font_axis = {'family' : 'Times New Roman',
+'weight' : 'normal',
+'size'   : 16,
+}
+font_legend = {'family' : 'Times New Roman',
+'weight' : 'normal',
+'size'   : 14,
+}
+
 parser = argparse.ArgumentParser(description='analyze the csv file in folder')
-parser.add_argument('--input_filename', '-i', required = True,
-        help='xxx.db, input filename please')
+parser.add_argument('--folder', '-f', required = True,
+        help='input folder please')
 args = parser.parse_args()
 print(args)
 
-results = np.loadtxt(os.path.join(args.input_filename, 'log.csv'), skiprows=1, delimiter=',')
+results = np.loadtxt(os.path.join(args.folder, 'log.csv'), skiprows=1, delimiter=',')
 
 time = results[:,0]-results[0,0]
 learning_rate = results[:,1]
@@ -22,16 +31,19 @@ val_mae = results[:,4]
 print('Final validation MAE:', np.round(val_mae[-1], 2), 'eV =',
               np.round(val_mae[-1] / (kcal/mol), 2), 'kcal/mol')
 
-plt.figure(figsize=(14,5))
-plt.subplot(1,2,1)
-plt.plot(time, val_loss, label='Validation')
-plt.plot(time, train_loss, label='Train')
+# plt.figure(figsize=(14,5))
+# plt.subplot(1,2,1)
+plt.plot(time/3600, val_loss, label='Validation')
+plt.plot(time/3600, train_loss, label='Train')
 plt.yscale('log')
-plt.ylabel('Loss [eV]')
-plt.xlabel('Time [s]')
-plt.legend()
-plt.subplot(1,2,2)
-plt.plot(time, val_mae)
-plt.ylabel('mean abs. error [eV]')
-plt.xlabel('Time [s]')
+plt.ylabel('Loss/eV', font_axis)
+plt.xlabel('Time/(h)', font_axis)
+plt.tick_params(labelsize=16)
+plt.legend(fontsize=16)
+# plt.subplot(1,2,2)
+# plt.plot(time, val_mae)
+# plt.ylabel('mean abs. error [eV]')
+# plt.xlabel('Time [s]')
+
+plt.subplots_adjust(bottom=0.12, right=0.986, left=0.124, top=0.986)
 plt.show()
